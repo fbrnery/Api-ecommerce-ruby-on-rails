2b166dc62e8181709a88776c9eb3342a0c793b24
Fabricio Miranda <fbrnery@gmail.com>
2023-03-10 18:21:38 -0300

    Adicionando Busca por nome nos Models

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,6 +1,7 @@
 require "rails_helper"
 
 RSpec.describe Category, type: :model do
+  it_behaves_like "name searchable concern", :category
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
   it { is_expected.to have_many(:product_categories).dependent(:destroy) }
